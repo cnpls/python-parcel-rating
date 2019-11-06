cfd06ff9bd20e7c6101e35c8e0b838347b52d607
christopherpryer <christophpryer@gmail.com>
2019-11-06 16:29:43 +0000

    completed first pass parcel rating

--- tests.py
+++ tests.py
@@ -12,7 +12,20 @@ INSTANCE = os.path.join(ROOT, 'instance')
 class Config:
     fedex = FEDEX_OBJ
     data = pd.read_csv(os.path.join(ROOT, 'example_service_config.csv'))
+    debug = False
 
 def test_init():
     app = App(Config, storage_dir=INSTANCE)
     assert app
+
+def test_batch():
+    app = App(Config, storage_dir=INSTANCE)
+    assert app
+
+    shipments = pd.read_csv(os.path.join(ROOT, 'example_parcel_shipments.csv'))
+    app.rate(shipments)
+    assert not app.shipments.empty
+
+if __name__ == '__main__':
+    test_init()
+    test_batch()
